b20ddfecf089047e48e496f539a0c62fb008bc6b
Salehahmedeus <97753281+Salehahmedeus@users.noreply.github.com>
2024-11-01 18:47:45 +0200

    launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:esafy/providers/auth_provider.dart';
 import 'package:esafy/providers/base_provider.dart';
 import 'package:esafy/screens/splash_screen.dart';
 
-
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
@@ -24,7 +27,6 @@ class MyApp extends StatelessWidget {
               create: (context) => AuthenticationProvider()),
         ],
         child: const GetMaterialApp(
-          debugShowCheckedModeBanner: false,
           home: SplashScreen(),
         ));
   }
